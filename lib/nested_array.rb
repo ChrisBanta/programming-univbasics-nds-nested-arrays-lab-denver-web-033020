a41f3a2a71b390bd1fa463a8adefe25f1a8c8dd3
Chris Banta <chrisbanta1@gmail.com>
2020-03-12 20:50:54 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -39,6 +39,9 @@ produce_storage_room = [
 end
 
 def matrix_lookup(matrix, row, column)
+  return matrix[row][column]
+
+
   # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
 end
